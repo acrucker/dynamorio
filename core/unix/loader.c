acrucker/dynamorio

commit 33702a5f3da2da1a26c21ce90f9e74af670399e2

i#1717 C++ exceptions in clients: emulate dl_iterate_phdr
Redirects client and private library calls to dl_iterate_phdr() to a new
routine redirect_dl_iterate_phdr() which provides the same data.  This
fixes problems in handling C++ exceptions thrown by private libraries.

Adds a new client.exception test.

This fixes the issue for Linux, but Windows still has problems.

Review-URL: https://codereview.appspot.com/247450044

core/unix/loader.c
@@ -46,6 +46,9 @@
 #ifdef LINUX
 # include "include/syscall.h"
 # include "memquery.h"
+# define _GNU_SOURCE 1
+# define __USE_GNU 1
+# include <link.h> /* struct dl_phdr_info, must be prior to dlfcn.h */
 #else
 # include <sys/syscall.h>
 #endif
@@ -1358,6 +1361,35 @@ redirect____tls_get_addr()
             tls_info.offs[ti->ti_module] + ti->ti_offset);
 }
 
+int
+redirect_dl_iterate_phdr(int (*callback)(struct dl_phdr_info *info,
+                                         size_t size, void *data),
+                         void *data)
+{
+    int res = 0;
+    struct dl_phdr_info info;
+    privmod_t *mod;
+    acquire_recursive_lock(&privload_lock);
+    for (mod = privload_first_module(); mod != NULL; mod = privload_next_module(mod)) {
+        ELF_HEADER_TYPE *elf_hdr = (ELF_HEADER_TYPE *) mod->base;
+        os_privmod_data_t *opd = (os_privmod_data_t *) mod->os_privmod_data;
+        /* We do want to include externally loaded (if any) and clients as
+         * clients can contain C++ exception code, which will call here.
+         */
+        if (mod->base == get_dynamorio_dll_start())
+            continue;
+        info.dlpi_addr = opd->load_delta;
+        info.dlpi_name = mod->name;
+        info.dlpi_phdr = (ELF_PROGRAM_HEADER_TYPE *)(mod->base + elf_hdr->e_phoff);
+        info.dlpi_phnum = elf_hdr->e_phnum;
+        res = callback(&info, sizeof(info), data);
+        if (res != 0)
+            break;
+    }
+    release_recursive_lock(&privload_lock);
+    return res;
+}
+
 typedef struct _redirect_import_t {
     const char *name;
     app_pc func;
@@ -1374,6 +1406,8 @@ static const redirect_import_t redirect_imports[] = {
      */
     {"__tls_get_addr", (app_pc)redirect___tls_get_addr},
     {"___tls_get_addr", (app_pc)redirect____tls_get_addr},
+    /* i#1717: C++ exceptions call this */
+    {"dl_iterate_phdr", (app_pc)redirect_dl_iterate_phdr},
 };
 #define REDIRECT_IMPORTS_NUM (sizeof(redirect_imports)/sizeof(redirect_imports[0]))
 

core/unix/memquery_emulate.c
@@ -115,7 +115,7 @@ static int
 dl_iterate_get_path_cb(struct dl_phdr_info *info, size_t size, void *data)
 {
     dl_iterate_data_t *iter_data = (dl_iterate_data_t *) data;
-    /* info->dlpi->addr is offset from preferred so we need to calculate the
+    /* info->dlpi_addr is offset from preferred so we need to calculate the
      * absolute address of the base.
      * we can calculate the absolute address of the first segment, but ELF
      * doesn't seem to guarantee that either the elf header (base of

suite/tests/CMakeLists.txt
@@ -1571,6 +1571,9 @@ if (CLIENT_INTERFACE)
     tobuild_ci(client.count-bbs client-interface/count-bbs.c "" "" "")
   endif (NOT ARM)
   tobuild_ci(client.app_inscount client-interface/app_inscount.c "" "" "")
+  if (NOT WIN32) # FIXME i#1717: add Windows client C++ EH support
+    tobuild_ci(client.exception client-interface/exception.cpp "" "" "")
+  endif ()
 
   if (NOT ARM)
     set(DynamoRIO_USE_LIBC OFF)

suite/tests/client-interface/exception.dll.cpp
@@ -1,0 +1,78 @@
+/* **********************************************************
+ * Copyright (c) 2015 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#include "dr_api.h"
+#include <string>
+#include <stdexcept>
+#ifdef LINUX
+# define _GNU_SOURCE 1
+# define __USE_GNU 1
+# include <link.h> /* struct dl_phdr_info */
+#endif
+#ifdef WINDOWS
+# pragma warning(disable : 4100) /* 'id' : unreferenced formal parameter */
+# pragma warning(disable : 4702) /* unreachable code */
+#endif
+
+#ifdef LINUX
+static int
+dl_iterate_cb(struct dl_phdr_info *info, size_t size, void *data)
+{
+# ifdef VERBOSE
+    dr_printf("dl_iterate_cb: addr="PFX" hdrs="PFX" num=%d name=%s\n",
+              info->dlpi_addr, info->dlpi_phdr, info->dlpi_phnum, info->dlpi_name);
+# endif
+    return 0; /* continue */
+}
+#endif
+
+DR_EXPORT
+void
+dr_init(client_id_t id)
+{
+#ifdef LINUX
+    int res = dl_iterate_phdr(dl_iterate_cb, NULL);
+    dr_printf("dl_iterate_phdr returned %d\n", res);
+#else
+    /* make expect file cross-platform */
+    dr_printf("dl_iterate_phdr returned 0\n");
+#endif
+
+    try {
+        dr_printf("about to throw\n");
+        throw std::runtime_error("test throw");
+        dr_printf("should not get here\n");
+    } catch (std::runtime_error &e) {
+        dr_printf("caught runtime_error %s\n", e.what());
+    }
+    dr_printf("all done\n");
+}

suite/tests/client-interface/exception.expect
@@ -1,0 +1,5 @@
+dl_iterate_phdr returned 0
+about to throw
+caught runtime_error test throw
+all done
+Hello, world!

commit 18993cf26597564162b805931bfbced329518d16

i#1717 C++ exceptions in clients: fix MacOS build
Fixes MacOS build breakage caused by 33702a5 due to dl_iterate_phdr() not
existing on MacOS.

Review-URL: https://codereview.appspot.com/259140043

core/unix/loader.c
@@ -56,7 +56,7 @@
 
 #include <dlfcn.h>      /* dlsym */
 #ifdef LINUX
-#  include <sys/prctl.h>  /* PR_SET_NAME */
+# include <sys/prctl.h>  /* PR_SET_NAME */
 #endif
 #include <string.h>     /* strcmp */
 #include <stdlib.h>     /* getenv */
@@ -1375,6 +1375,7 @@ redirect____tls_get_addr()
             tls_info.offs[ti->ti_module] + ti->ti_offset);
 }
 
+#ifdef LINUX
 int
 redirect_dl_iterate_phdr(int (*callback)(struct dl_phdr_info *info,
                                          size_t size, void *data),
@@ -1403,6 +1404,7 @@ redirect_dl_iterate_phdr(int (*callback)(struct dl_phdr_info *info,
     release_recursive_lock(&privload_lock);
     return res;
 }
+#endif
 
 typedef struct _redirect_import_t {
     const char *name;
@@ -1420,8 +1422,10 @@ static const redirect_import_t redirect_imports[] = {
      */
     {"__tls_get_addr", (app_pc)redirect___tls_get_addr},
     {"___tls_get_addr", (app_pc)redirect____tls_get_addr},
+#ifdef LINUX
     /* i#1717: C++ exceptions call this */
     {"dl_iterate_phdr", (app_pc)redirect_dl_iterate_phdr},
+#endif
     /* We need these for clients that don't use libc (i#1747) */
     {"strlen", (app_pc)strlen},
     {"wcslen", (app_pc)wcslen},