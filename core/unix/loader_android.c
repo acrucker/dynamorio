acrucker/dynamorio

commit 8f651e1364b07153d4e91428379e77fbdf811ac9

i#1701 port to Android: add support for loading a private Bionic
Refactors loader TLS setup out of core/unix/loader.c into
core/unix/loader_{linux,android}.c.

Implements Android TLS support by emulating the special setup done by
Android's linker for Bionic:

+ Initializes the internal pthread data structure for the initial thread.
  This unfortunately requires specifying the precise data structure (in
  core/unix/include/android_linker.h), which adds a potentially fragile
  dependence.

+ Uses the TLS field of the initial thread structure as the TLS memory,
  rather than an mmapped region.

+ Appends our own field to the end of the pthread structure for use in
  storing a pointer to DR's own TLS region.

Re-enables libc and clients that need libc for samples.

Updates the docs.

Review-URL: https://codereview.appspot.com/286910043

core/unix/loader_android.c
@@ -1,0 +1,116 @@
+/* *******************************************************************************
+ * Copyright (c) 2016 Google, Inc.  All rights reserved.
+ * *******************************************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/*
+ * loader_android.c: Android-specific private loader code
+ */
+
+#include "../globals.h"
+#include "../module_shared.h"
+#include "include/syscall.h"
+#include "tls.h"
+#include "include/android_linker.h"
+#include <stddef.h>
+
+/****************************************************************************
+ * Thread Local Storage
+ */
+
+static android_kernel_args_t kernel_args;
+static android_pthread_internal_t init_thread;
+
+extern void *kernel_init_sp;
+
+#ifdef X64
+# define PTHREAD_TLS_OFFS 176
+#else
+# define PTHREAD_TLS_OFFS  72
+#endif
+
+void
+privload_mod_tls_init(privmod_t *mod)
+{
+    /* Android does not yet support per-module TLS */
+}
+
+/* We expect app_tls to be either NULL or garbage, as we have early injection */
+void *
+privload_tls_init(void *app_tls)
+{
+    char **e;
+
+    /* We have to exactly duplicate the offset of key fields in Android's
+     * pthread_internal_t struct.
+     */
+    ASSERT(PTHREAD_TLS_OFFS == offsetof(android_pthread_internal_t, tls));
+    ASSERT(DR_TLS_BASE_OFFSET == offsetof(android_pthread_internal_t, dr_tls_base) -
+           PTHREAD_TLS_OFFS /* the self slot */);
+
+    init_thread.tid = dynamorio_syscall(SYS_set_tid_address, 1, &init_thread.tid);
+    init_thread.cached_pid_ = init_thread.tid;
+
+    /* init_thread.attr is set to all 0 (sched is SCHED_NORMAL==0, and sizes are
+     * zeroed out)
+     */
+
+    /* init_thread.join_state is set to 0 (THREAD_NOT_JOINED) */
+
+    init_thread.tls[ANDROID_TLS_SLOT_SELF] = init_thread.tls;
+    init_thread.tls[ANDROID_TLS_SLOT_THREAD_ID] = &init_thread;
+    /* tls[TLS_SLOT_STACK_GUARD] is set to 0 */
+
+    /* Set up the data struct pointing at kernel args that Bionic expects */
+    kernel_args.argc = *(int *)kernel_init_sp;
+    kernel_args.argv = (char **)kernel_init_sp + 1;
+    kernel_args.envp = kernel_args.argv + kernel_args.argc + 1;
+    /* The aux vector is after the last environment pointer. */
+    for (e = kernel_args.envp; *e != NULL; e++)
+        ; /* nothing */
+    kernel_args.auxv = (ELF_AUXV_TYPE *)(e + 1);
+    init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT] = &kernel_args;
+
+    /* We use our own alternate signal stack */
+
+    LOG(GLOBAL, LOG_LOADER, 2, "%s: kernel sp is "PFX"; TLS set to "PFX"\n",
+        __FUNCTION__, init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT],
+        init_thread.tls[ANDROID_TLS_SLOT_SELF]);
+
+    /* Android does not yet support per-module TLS */
+
+    return init_thread.tls[ANDROID_TLS_SLOT_SELF];
+}
+
+void
+privload_tls_exit(void *dr_tp)
+{
+    /* nothing to do */
+}

api/docs/release.dox
@@ -140,6 +140,8 @@ following minor compatibility changes:
 
 The changes between version \DR_VERSION and 6.0.0 include:
 
+ - Added support for C++ clients on Android via private loader support for
+   Bionic.
  - opnd_size_in_bytes() and opnd_size_in_bits() no longer accept DR_REG_
    constants as input (the caller must first call reg_get_size()).
    This was true in the 6.0.0 release but the documentation and the
@@ -1289,8 +1291,7 @@ not supported.
   - This release of DynamoRIO supports 32-bit applications running on
     32-bit Linux operating systems on AArch32 ARMv7 and ARMv8 hardware.
   - This release of DynamoRIO has experimental support for 32-bit Android
-    applications running on AArch32 ARMv7 and ARMv8 hardware with C
-    clients (but not yet C++ clients).
+    applications running on AArch32 ARMv7 and ARMv8 hardware.
   - This release of DynamoRIO has support for running 32-bit
     Linux applications on 64-bit Linux operating systems on
     AMD64-compatible hardware.

api/samples/CMakeLists.txt
@@ -1,5 +1,5 @@
 # **********************************************************
-# Copyright (c) 2010-2015 Google, Inc.    All rights reserved.
+# Copyright (c) 2010-2016 Google, Inc.    All rights reserved.
 # Copyright (c) 2009-2010 VMware, Inc.    All rights reserved.
 # **********************************************************
 
@@ -125,10 +125,6 @@ endif (DEBUG)
 # For C clients that only rely on the DR API and not on any 3rd party
 # library routines, we could shrink the size of the client binary
 # by disabling libc via "set(DynamoRIO_USE_LIBC OFF)".
-if (ANDROID)
-  # Android support does not yet include private libc support
-  set(DynamoRIO_USE_LIBC OFF)
-endif ()
 
 if (NOT DEFINED DynamoRIO_DIR)
   set(DynamoRIO_DIR "${PROJECT_SOURCE_DIR}/../cmake" CACHE PATH
@@ -237,11 +233,8 @@ add_sample_client(signal      "signal.c"        "drmgr")
 add_sample_client(syscall     "syscall.c"       "drmgr")
 add_sample_client(inline      "inline.c"        "")
 add_sample_client(inscount    "inscount.c"      "drmgr")
-if (NOT ANDROID)
-  # FIXME i#1701: no C or C++ library support yet for clients
-  add_sample_client(opcodes     "opcodes.c"       "drx")
-  add_sample_client(stl_test    "stl_test.cpp"    "")
-endif ()
+add_sample_client(opcodes     "opcodes.c"       "drx")
+add_sample_client(stl_test    "stl_test.cpp"    "")
 # add utils.h for installation  # NON-PUBLIC
 set(srcs ${srcs} "utils.h")     # NON-PUBLIC
 

core/CMakeLists.txt
@@ -1,5 +1,5 @@
 # **********************************************************
-# Copyright (c) 2010-2015 Google, Inc.    All rights reserved.
+# Copyright (c) 2010-2016 Google, Inc.    All rights reserved.
 # Copyright (c) 2009-2010 VMware, Inc.    All rights reserved.
 # **********************************************************
 
@@ -257,6 +257,11 @@ if (UNIX)
     unix/loader.c
     )
   if (LINUX)
+    if (ANDROID)
+      set(OS_SRCS ${OS_SRCS} unix/loader_android.c)
+    else ()
+      set(OS_SRCS ${OS_SRCS} unix/loader_linux.c)
+    endif ()
     set(OS_SRCS ${OS_SRCS} unix/memquery_linux.c)
     set(OS_SRCS ${OS_SRCS} unix/memquery.c)
     set(OS_SRCS ${OS_SRCS} unix/memcache.c)

core/unix/include/android_linker.h
@@ -1,0 +1,103 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was generated from Android headers to make
+ ***   information necessary for a loader to call into the Bionic
+ ***   C library available to DynamoRIO.  It contains only constants,
+ ***   structures, and macros generated from the original header, and
+ ***   thus, contains no copyrightable information.
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+
+#ifndef _ANDROID_LINKER_H_
+#define _ANDROID_LINKER_H_ 1
+
+#include <unistd.h>
+#include "module_private.h"
+
+/* i#1701: our loader must emulate what Android's dynamic linker does
+ * when initializing the initial internal pthread data struct and passing
+ * kernel arguments to Bionic.
+ *
+ * These definitions are from Android's pthread_internal.h,
+ * pthread_types.h, pthread.h, and KernelArgumentBlock.h.
+ * The type names are changed to our equivalents.
+ */
+
+typedef struct {
+    uint flags;
+    void* stack_base;
+    size_t stack_size;
+    size_t guard_size;
+    int sched_policy;
+    int sched_priority;
+#ifdef __LP64__
+    char __reserved[16];
+#endif
+} android_pthread_attr_t;
+
+typedef struct {
+#if defined(__LP64__)
+    int __private[10];
+#else
+    int __private[1];
+#endif
+} android_pthread_mutex_t;
+
+enum android_join_state_t {
+    ANDROID_THREAD_NOT_JOINED,
+    ANDROID_THREAD_EXITED_NOT_JOINED,
+    ANDROID_THREAD_JOINED,
+    ANDROID_THREAD_DETACHED
+};
+
+enum {
+    ANDROID_TLS_SLOT_SELF = 0,
+    ANDROID_TLS_SLOT_THREAD_ID,
+    ANDROID_TLS_SLOT_ERRNO,
+    ANDROID_TLS_SLOT_OPENGL_API = 3,
+    ANDROID_TLS_SLOT_OPENGL = 4,
+    ANDROID_TLS_SLOT_BIONIC_PREINIT = ANDROID_TLS_SLOT_OPENGL_API,
+    ANDROID_TLS_SLOT_STACK_GUARD = 5,
+    ANDROID_TLS_SLOT_DLERROR,
+    ANDROID_BIONIC_TLS_SLOTS
+};
+
+#define ANDROID_RESERVED_KEYS 12
+#define ANDROID_PTHREAD_KEYS_MAX 128
+#define ANDROID_PTHREAD_KEYS_TOT (ANDROID_RESERVED_KEYS + ANDROID_PTHREAD_KEYS_MAX)
+
+typedef struct _android_pthread_internal_t {
+    struct _pthread_internal_t* next;
+    struct _pthread_internal_t* prev;
+    pid_t tid;
+    pid_t cached_pid_;
+    android_pthread_attr_t attr;
+    int /* really std::atomic<ThreadJoinState> */ join_state;
+    void *cleanup_stack;
+    void *(*start_routine)(void*);
+    void *start_routine_arg;
+    void *return_value;
+    void *alternate_signal_stack;
+    android_pthread_mutex_t startup_handshake_mutex;
+    size_t mmap_size;
+    /* The TLS register points here, to slot #0 (ANDROID_TLS_SLOT_SELF) */
+    void *tls[ANDROID_BIONIC_TLS_SLOTS];
+    int /* really pthread_key_t */ pthread_keys[ANDROID_PTHREAD_KEYS_TOT];
+    /* We use this to store the DR TLS base */
+    void *dr_tls_base;
+} android_pthread_internal_t;
+
+
+/* Adapated from class KernelArgumentBlock */
+typedef struct _android_kernel_args_t {
+    int argc;
+    char** argv;
+    char** envp;
+    ELF_AUXV_TYPE *auxv;
+    char *abort_message_ptr;
+} android_kernel_args_t;
+
+
+#endif /* _ANDROID_LINKER_H_ */

commit 2d432e0a8d9eba5d0978228b6cc782db9f3e74a5

i#1875: fix multi-thread Android bug
Fixes a regression from 8f651e1 where the Bionic support only included
primary thread support.  Adds secondary thread support by mmapping a new
pthread data structure and filling in key fields.

Fixes #1875

Review-URL: https://codereview.appspot.com/289220043

core/unix/loader_android.c
@@ -62,12 +62,10 @@ privload_mod_tls_init(privmod_t *mod)
     /* Android does not yet support per-module TLS */
 }
 
-/* We expect app_tls to be either NULL or garbage, as we have early injection */
 void *
 privload_tls_init(void *app_tls)
 {
-    char **e;
-
+    void *res;
     /* We have to exactly duplicate the offset of key fields in Android's
      * pthread_internal_t struct.
      */
@@ -75,44 +73,71 @@ privload_tls_init(void *app_tls)
     ASSERT(DR_TLS_BASE_OFFSET == offsetof(android_pthread_internal_t, dr_tls_base) -
            PTHREAD_TLS_OFFS /* the self slot */);
 
-    init_thread.tid = dynamorio_syscall(SYS_set_tid_address, 1, &init_thread.tid);
-    init_thread.cached_pid_ = init_thread.tid;
+    if (!dynamo_initialized) {
+        char **e;
+        /* We have to duplicate the pthread setup that the Android loader does.
+         * We expect app_tls to be either NULL or garbage, as we have early injection.
+         */
+        init_thread.tid = dynamorio_syscall(SYS_set_tid_address, 1, &init_thread.tid);
+        init_thread.cached_pid_ = init_thread.tid;
 
-    /* init_thread.attr is set to all 0 (sched is SCHED_NORMAL==0, and sizes are
-     * zeroed out)
-     */
+        /* init_thread.attr is set to all 0 (sched is SCHED_NORMAL==0, and sizes are
+         * zeroed out)
+         */
 
-    /* init_thread.join_state is set to 0 (THREAD_NOT_JOINED) */
+        /* init_thread.join_state is set to 0 (THREAD_NOT_JOINED) */
 
-    init_thread.tls[ANDROID_TLS_SLOT_SELF] = init_thread.tls;
-    init_thread.tls[ANDROID_TLS_SLOT_THREAD_ID] = &init_thread;
-    /* tls[TLS_SLOT_STACK_GUARD] is set to 0 */
+        init_thread.tls[ANDROID_TLS_SLOT_SELF] = init_thread.tls;
+        init_thread.tls[ANDROID_TLS_SLOT_THREAD_ID] = &init_thread;
+        /* tls[TLS_SLOT_STACK_GUARD] is set to 0 */
 
-    /* Set up the data struct pointing at kernel args that Bionic expects */
-    kernel_args.argc = *(int *)kernel_init_sp;
-    kernel_args.argv = (char **)kernel_init_sp + 1;
-    kernel_args.envp = kernel_args.argv + kernel_args.argc + 1;
-    /* The aux vector is after the last environment pointer. */
-    for (e = kernel_args.envp; *e != NULL; e++)
-        ; /* nothing */
-    kernel_args.auxv = (ELF_AUXV_TYPE *)(e + 1);
-    init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT] = &kernel_args;
+        /* Set up the data struct pointing at kernel args that Bionic expects */
+        kernel_args.argc = *(int *)kernel_init_sp;
+        kernel_args.argv = (char **)kernel_init_sp + 1;
+        kernel_args.envp = kernel_args.argv + kernel_args.argc + 1;
+        /* The aux vector is after the last environment pointer. */
+        for (e = kernel_args.envp; *e != NULL; e++)
+            ; /* nothing */
+        kernel_args.auxv = (ELF_AUXV_TYPE *)(e + 1);
+        init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT] = &kernel_args;
 
-    /* We use our own alternate signal stack */
+        /* We use our own alternate signal stack */
 
-    LOG(GLOBAL, LOG_LOADER, 2, "%s: kernel sp is "PFX"; TLS set to "PFX"\n",
-        __FUNCTION__, init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT],
-        init_thread.tls[ANDROID_TLS_SLOT_SELF]);
+        LOG(GLOBAL, LOG_LOADER, 2, "%s: kernel sp is "PFX"; TLS set to "PFX"\n",
+            __FUNCTION__, init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT],
+            init_thread.tls[ANDROID_TLS_SLOT_SELF]);
+
+        res = init_thread.tls[ANDROID_TLS_SLOT_SELF];
+    } else {
+        android_pthread_internal_t *thrd;
+        res = heap_mmap(ALIGN_FORWARD(sizeof(android_pthread_internal_t), PAGE_SIZE));
+        LOG(GLOBAL, LOG_LOADER, 2, "%s: allocated new TLS at "PFX"; copying from "PFX"\n",
+            __FUNCTION__, res, app_tls);
+        if (app_tls != NULL)
+            memcpy(res, app_tls, sizeof(android_pthread_internal_t));
+        thrd = (android_pthread_internal_t *) res;
+        thrd->tls[ANDROID_TLS_SLOT_SELF] = thrd->tls;
+        thrd->tls[ANDROID_TLS_SLOT_THREAD_ID] = thrd;
+        thrd->tid = get_thread_id();
+        thrd->dr_tls_base = NULL;
+        res = thrd->tls[ANDROID_TLS_SLOT_SELF];
+        LOG(GLOBAL, LOG_LOADER, 2, "%s: TLS set to "PFX"\n",
+            __FUNCTION__, thrd->tls[ANDROID_TLS_SLOT_SELF]);
+    }
 
     /* Android does not yet support per-module TLS */
 
-    return init_thread.tls[ANDROID_TLS_SLOT_SELF];
+    return res;
 }
 
 void
 privload_tls_exit(void *dr_tp)
 {
-    /* nothing to do */
+    byte *alloc;
+    if (dr_tp == NULL || dr_tp == init_thread.tls)
+        return;
+    alloc = (byte *)dr_tp - offsetof(android_pthread_internal_t, tls);
+    heap_munmap(alloc, ALIGN_FORWARD(sizeof(android_pthread_internal_t), PAGE_SIZE));
 }
 
 /* For standalone lib usage (i#1862: the Android loader passes